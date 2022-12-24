3282fcdca7236fe41d5e8bd2fb820580dd666c74
Niki313 <107651537+Niki313@users.noreply.github.com>
2022-12-24 20:20:10 +0000

    Connecting yarn and axios to the site

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import Weather from './Weather';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>jjjjjjjjj </p>
+        <Weather city="Tokyo"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,32 @@
+import React  from "react";
+import axios from "axios";
+import { Audio } from 'react-loader-spinner';
+
+
+export default function Weather(props) {
+
+    function handleResponse (response){
+        alert(`The weather in ${response.data.name} is ${Math.round(response.data.main.temp)}°C`);
+    }
+   
+
+        let apiKey = "3c3046eb3665ca592e70fff5ccda526b";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+        axios.get(apiUrl).then(handleResponse);
+
+        return (
+            <div>
+                <Audio
+                    height="80"
+                    width="80"
+                    radius="9"
+                    color="green"
+                    ariaLabel="loading"
+                    wrapperStyle
+                    wrapperClass
+                />   
+            </div>
+                );
+}
+
